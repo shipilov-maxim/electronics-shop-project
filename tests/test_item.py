e9ec59e2185d2509c509593073b03b3f140b3ee3
shipilov-maxim <skam3840@gmail.com>
2023-10-08 20:42:06 +0400

    make class

--- tests/test_item.py
+++ tests/test_item.py
@@ -1,1 +1,19 @@
 """Здесь надо написать тесты с использованием pytest для модуля item."""
+import pytest
+from ..src.item import Item
+
+
+@pytest.fixture
+def item():
+    return Item("Смартфон", 10000, 20)
+
+
+Item.pay_rate = 0.8
+
+
+def test_calculate_total_price(item):
+    assert item.calculate_total_price() == 200000
+
+
+def test_apply_discount(item):
+    assert item.apply_discount() == 8000.0
